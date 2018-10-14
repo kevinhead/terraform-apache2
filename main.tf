7a044c02449cf621c94988d5d836e438b3d67e31
Kevin Head <kevinleehead@gmail.com>
2018-10-14 13:33:14 -0500

    multiple sites

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
-module "example" {
+module "foo" {
   source = "modules/vhost"
   
-  server_name = "example.com"
+  server_name = "foo.com"
+}
+
+module "bar" {
+  source = "modules/vhost"
+  
+  server_name = "bar.com"
 }
